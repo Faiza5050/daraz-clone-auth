f202fe31d6c3d2da9572466676f7076bcb499ca8
Faiza5050 <faizakanwal5050@gmail.com>
2025-02-16 16:21:31 +0500

    Fix build script

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.html", // Saare HTML files inside 'src' folder
-    "./src/**/*.js"    // JS files agar aap dynamic classes use kar rahe hain
+    "./src/**/*.html",
+    "./src/**/*.js",
+    "./src/**/*.css"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
